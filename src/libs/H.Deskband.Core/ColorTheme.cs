a1d864ab30d1503517751491a45da1dda2a39371
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-16 16:09:00 +1000

    Added H.Deskband.Core project.

---
+++ src/libs/H.Deskband.Core/ColorTheme.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace H.SearchDeskBand
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class ColorTheme
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color TextColor { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color BackgroundColor { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color ActiveColor { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color InactiveColor { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color MouseOverColor { get; set; }
+    }
+}
