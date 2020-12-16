dd2577bab089935048ec128eb9e6e52419995d1c
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-16 16:01:17 +1000

    Initial commit.

---
+++ src/apps/H.DeskBand.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+using H.SearchDeskBand.TestApp;
+
+var thread = new Thread(() =>
+{
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+    Application.Run(new MainForm());
+});
+thread.SetApartmentState(ApartmentState.STA);
+thread.Start();
---
+++ src/libs/H.Deskband/SearchDeskBand.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+using SharpShell.Attributes;
+using SharpShell.SharpDeskBand;
+
+namespace H.SearchDeskBand
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ComVisible(true)]
+    [DisplayName("Home Center Search")]
+    [Guid("AE9E11C0-E4FD-4F96-B9B6-66CC76C2B45D")]
+    [ProgId("H.NET.SearchDeskBand")]
+    public sealed class WebSearchDeskBand : SharpDeskBand
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override System.Windows.Forms.UserControl CreateDeskBand()
+        {
+            return new DeskBandControl();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override BandOptions GetBandOptions()
+        {
+            return new()
+            {
+                HasVariableHeight = false,
+                IsSunken = false,
+                ShowTitle = true,
+                Title = "Home Center Search",
+                UseBackgroundColour = false,
+                AlwaysShowGripper = true
+            };
+        }
+    }
+}
